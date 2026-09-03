8223138d7b5af782c84eaf4d699a5fc4f809bdbf
stan0312 <stan0312@users.noreply.github.com>
2026-09-04 05:18:41 +0800

    pin media-kit to official 7a21356; fix broken symlinks (media_kit_video ios/macos)

--- packages/media_kit_video/macos/Classes/plugin/common/ResizableTextureProtocol.swift
+++ packages/media_kit_video/macos/Classes/plugin/common/ResizableTextureProtocol.swift
@@ -1,1 +1,10 @@
-../../../../common/darwin/Classes/plugin/ResizableTextureProtocol.swift
+#if canImport(Flutter)
+  import Flutter
+#elseif canImport(FlutterMacOS)
+  import FlutterMacOS
+#endif
+
+public protocol ResizableTextureProtocol: NSObject, FlutterTexture {
+  func resize(_ size: CGSize)
+  func render(_ size: CGSize)
+}
--- third_party/media-kit-upstream/media_kit_video/macos/Classes/stub/common/MediaKitVideoPlugin.swift
+++ third_party/media-kit-upstream/media_kit_video/macos/Classes/stub/common/MediaKitVideoPlugin.swift
@@ -1,1 +1,9 @@
-../../../../common/darwin/Classes/stub/MediaKitVideoPlugin.swift
+#if canImport(Flutter)
+  import Flutter
+#elseif canImport(FlutterMacOS)
+  import FlutterMacOS
+#endif
+
+public class MediaKitVideoPlugin: NSObject, FlutterPlugin {
+  public static func register(with _: FlutterPluginRegistrar) {}
+}
