a2ca48437e0760f51aa139473912b6b487d2770b
stan0312 <stan0312@users.noreply.github.com>
2026-09-20 11:47:30 +0800

    修复cacheExtent编译错误，改用allowImplicitScrolling预加载

--- lib/pages/emby_swipe_page.dart
+++ lib/pages/emby_swipe_page.dart
@@ -958,7 +958,7 @@ class _EmbySwipePageState extends State<EmbySwipePage>
                 controller: _pageController,
                 scrollDirection: Axis.vertical,
                 itemCount: _items.length,
-                cacheExtent: MediaQuery.of(context).size.height,
+                allowImplicitScrolling: true,
                 onPageChanged: (i) {
                   setState(() => _currentIndex = i);
                   if (i >= 0 && i < _items.length) {
